f73f5a74116d0567e9c7f50849cb0d1c070733b5
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-11-14 16:51:39 +0530

    [add] save user info to db

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -9,8 +9,11 @@ const useFirebase = () => {
     const [user, setUser] = useState({});
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(true);
+    const [saveUser, setSaveUser] = useState({});
 
     const register = (email, password, name, history) => {
+        const userInfo = { email, displayName: name };
+        // setSaveUser(userInfo);
         setLoading(true);
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
@@ -19,10 +22,11 @@ const useFirebase = () => {
                 updateProfile(auth.currentUser, {
                     displayName: name
                 }).then(() => {
-
+                    console.log('from register-', userInfo);
                 }).catch((error) => {
                     setError(error.message);
                 });
+                saveUserDb(userInfo);
                 setError('');
                 history.push('/');
             })
@@ -71,6 +75,18 @@ const useFirebase = () => {
             }).catch((error) => {
                 // An error happened.
             });
+    }
+
+    const saveUserDb = (userInfo) => {
+        console.log('run saveUserDb func', userInfo);
+        fetch('http://localhost:5000/users', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(userInfo)
+        }).then(res => res.json())
+            .then(data => console.log(data));
     }
 
 
