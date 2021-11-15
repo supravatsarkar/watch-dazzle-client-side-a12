ffcf297694450af358eab834ee081bac3b9c9774
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-11-15 13:47:14 +0530

    [add]  make admin

--- src/Pages/Dashboard/Admin/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/Admin/MakeAdmin/MakeAdmin.js
@@ -1,12 +1,59 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import { Container, Typography, TextField, Button, Paper } from '@mui/material';
+import React, { useState } from 'react';
 
 const MakeAdmin = () => {
+    const [email, setEmail] = useState('');
+
+    const handelOnBlur = e => {
+        const value = e.target.value;
+        console.log(value);
+        setEmail(value);
+    }
+
+    const handleOnSubmit = e => {
+        console.log('hit submit', email);
+        fetch(`http://localhost:5000/users/${email}`, {
+            method: 'PUT',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.matchedCount === 0) {
+                    alert('User not found');
+                }
+                else if (data.matchedCount && data.modifiedCount >= 1) {
+                    alert('make admin success');
+                    e.target.reset();
+                }
+                else if (data.matchedCount && data.modifiedCount === 0) {
+                    alert('User already an admin');
+                    e.target.reset();
+                }
+
+            });
+        e.preventDefault();
+    }
+
     return (
         <div>
             <Typography variant="h4" color="error.main" sx={{ fontWeight: 600, borderBottom: 1, borderColor: 'error.main', mb: 2 }}>
                 Make Admin
             </Typography>
+            <Container maxWidth="sm">
+                <Paper sx={{ p: 10 }}>
+                    <form onSubmit={handleOnSubmit}>
+                        <TextField
+                            onChange={handelOnBlur}
+                            name="email"
+                            id=""
+                            label="Enter Email Id"
+                            variant="outlined"
+                            sx={{ width: 1, mb: 1 }}
+                            color="warning"
+                            required />
+                        <Button type="submit" variant="contained" color="warning" sx={{ display: 'block', textAlign: 'center' }}>Make Admin</Button>
+                    </form>
+                </Paper>
+            </Container>
         </div>
     );
 };
